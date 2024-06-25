e991bd18c15a45366fd22c989eccad0f55083805
Aman Khanchandani <aman@Amans-MacBook-Air.local>
2024-06-25 22:33:36 +0530

    codecrafters submit [skip ci]

--- src/main/java/CLIHandler.java
+++ src/main/java/CLIHandler.java
@@ -1,17 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Set;
+
 public class CLIHandler {
+    static Set<String> validStrings =  Set.of("type", "echo", "exit");
     public static void handleRequest(String input) {
         if (input.equals("exit 0")) {
             System.exit(0);
         }
         String[] commandsParts = input.split(" ");
         if (commandsParts[0].equals("echo")) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 1; i < commandsParts.length; i++) {
-                sb.append(commandsParts[i]).append(" ");
+            System.out.println(fetchRemainingCommand(Arrays.stream(commandsParts).skip(1).toArray(String[]::new)));
+            return;
+        }
+        if (commandsParts[0].equals("type")) {
+            String command = fetchRemainingCommand(Arrays.stream(commandsParts).skip(1).toArray(String[]::new));
+            if(validStrings.contains(command.trim())){
+                System.out.println(command.trim() + " is a shell builtin");
+            } else {
+                System.out.println(command.trim() + ": not found");
             }
-            System.out.println(sb.substring(0, sb.length() - 1));
             return;
         }
         System.out.println(input + ": command not found");
+    }
+
+    private static String fetchRemainingCommand(String[] commandParts) {
+        StringBuilder sb = new StringBuilder();
+        for (String subString : commandParts) {
+            sb.append(subString).append(" ");
+        }
+        return sb.substring(0).trim();
     }
 }
